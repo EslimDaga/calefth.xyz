2ebac69136f17da7fdd9fa17726baf0dc8d38d4e
Eslim Daga <esliim9811@gmail.com>
2022-06-17 17:01:04 -0500

    Hmmm let's think about the functionalities :)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import Video from "./assets/video/pexels-pavel-danilyuk-7218609.mp4";
+import styled from "styled-components";
+import video from "./assets/video/pexels-pavel-danilyuk-7218609.mp4";
 import "./assets/css/main.css";
 
-function App() {
-  return <video autoPlay muted loop src={Video} className="main-video"></video>;
+const Video = styled.video`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+`;
+
+const Main = styled.div`
+  background: rgba(158, 212, 243, 0.6);
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+`;
+
+const App = () => {
+  return (
+    <>
+      <Video autoPlay muted loop src={video}></Video>
+      <Main></Main>
+    </>
+  );
 }
 
 export default App;
